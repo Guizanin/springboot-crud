1bab243614235de606e3e386af19d9f3db070a4d
Guizanin <guilhermecasazanin@gmail.com>
2024-01-25 14:06:29 -0300

    update table medicos

--- src/main/java/med/voll/api/medico/DadosListagemMedico.java
+++ src/main/java/med/voll/api/medico/DadosListagemMedico.java
@@ -1,12 +1,18 @@
 package med.voll.api.medico;
 
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PutMapping;
+
 public record DadosListagemMedico(
+        Long id,
         String nome,
         String email,
         String crm,
         Especialidade especialidade
 ) {
     public DadosListagemMedico(Medico medico) {
-        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+        this(medico.getId(),medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
     }
+
+
 }
